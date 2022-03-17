86d8f66d94f94e3dada9e7b0480e8cf9b403f3aa
Andres <andresvil@gmail.com>
2022-03-17 14:35:43 -0600

    edit rollup for ghpages

--- rollup.config.js
+++ rollup.config.js
@@ -69,7 +69,7 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser() && ghPages.publish('publish', (err) => {
+		production && terser() && ghPages.publish('public', (err) => {
 			console.log('published to github', err);
 		})
 	],
